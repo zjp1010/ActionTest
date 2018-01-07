4978f093bb59a5add4eb12661ad77a37bc70535b
general <zjp1010.good@163.com>
2018-01-07 20:33:18 +0800

    test

--- ActionTest/FirstViewController.m
+++ ActionTest/FirstViewController.m
@@ -21,9 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    NSLog(@"----------first    ");
-    
+        
 }
 
 -(void)viewWillAppear:(BOOL)animated
